739b513cffdfd399a6b9eca2cfae1a07db419aa2
Your Name <cdelcastillo@unsa.edu.pe>
2021-04-23 11:35:14 -0500

    Agregando multiplicacion y division

--- Calculator.java
+++ Calculator.java
@@ -1,12 +1,23 @@
 class Calculator {
   int add(int a, int b){
-	return a+b; }
+	return a+b; 
+  }
   int sub(int a, int b){
-	return a-b; }
-  int mul(int a, int b){ return 0; }
-  int div(int a, int b){ return 0; }
+	return a-b; 
+  }
+  int mul(int a, int b){ 
+	return a*b;
+  }
+  int div(int a, int b){
+	String c="ERROR"; 
+	if (b==0)
+	   return c;
+	else
+	   return a/b;
+  }
   int mod(int a, int b){
 	int cociente = a/b;
 	int mod = a-(cociente*b);
 	return mod;
+  }
 }
