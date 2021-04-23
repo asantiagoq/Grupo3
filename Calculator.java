cabefd4f908b80cca08f7dbd5591893ab617fc30
eduran-lab <eduranm@unsa.edu.pe>
2021-04-23 11:19:35 -0500

    agregando

--- Calculator.java
+++ Calculator.java
@@ -6,10 +6,9 @@ class Calculator {
   int mul(int a, int b){ return 0; }
   int div(int a, int b){ return 0; }
   int mod(int a, int b){
-		public int mod(int a, int b){
-			int cociente = a/b;
-			int mod = a-(cociente*b);
-			return mod;
-		}
+	public int mod(int a, int b){
+		int cociente = a/b;
+		int mod = a-(cociente*b);
+		return mod;
+	}
 }
-
