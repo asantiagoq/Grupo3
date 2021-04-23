2001bcfbe86aad00c577dbe758acab1de51ff51d
angelsantiago <asantiago@unsa.edu.pe>
2021-04-23 15:56:42 +0000

    Agregando la operacion modulo

--- Calculator.java
+++ Calculator.java
@@ -3,6 +3,11 @@ class Calculator {
   int sub(int a, int b){ return 0; }
   int mul(int a, int b){ return 0; }
   int div(int a, int b){ return 0; }
-  int mod(int a, int b){ return 0; }
+  int mod(int a, int b){
+		public int mod(int a, int b){
+			int cociente = a/b;
+			int mod = a-(cociente*b);
+			return mod;
+		}
 }
 
